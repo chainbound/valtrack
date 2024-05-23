25f5a731158e4160229317da00b441ace351b5cb
Jonas Bostoen <jonas.bostoen@hotmail.com>
2024-05-23 10:11:42 +0200

    feat(node): increase dial timeout

--- pkg/ethereum/node.go
+++ pkg/ethereum/node.go
@@ -213,7 +213,7 @@ func (n *Node) reconnectPeers() {
 		if time.Since(backoff.LastSeen) >= 30*time.Second && backoff.BackoffCounter < 10 {
 			n.log.Debug().Str("peer", pid.String()).Msg("Attempting to reconnect to peer")
 
-			ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+			ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 			err := n.host.Connect(ctx, backoff.AddrInfo)
 			cancel()
 
