cc3035400126080924fcdf13a321dd93c388305d
Jonas Bostoen <jonas.bostoen@hotmail.com>
2024-05-25 11:46:51 +0200

    feat(cli): add log-level flag

--- main.go
+++ main.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"context"
+	"flag"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/chainbound/valtrack/discovery"
+	"github.com/rs/zerolog"
+)
+
+var (
+	logLevel = flag.String("log-level", "info", "log level")
 )
 
 func main() {
+	flag.Parse()
+
 	disc, err := discovery.NewDiscovery()
 	if err != nil {
 		panic(err)
 	}
+
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+
+	level, _ := zerolog.ParseLevel(*logLevel)
+	zerolog.SetGlobalLevel(level)
 
 	ctx, cancel := context.WithCancel(context.Background())
 
